2bf6eb82b8420fae76fab462be8229f0057f3bc0
LKH <kangholee8512@gmail.com>
2023-05-02 06:57:15 +0900

    이벤트추가

--- app/src/main/java/com/cnc/kotlinbasic01_genie/MainActivity.kt
+++ app/src/main/java/com/cnc/kotlinbasic01_genie/MainActivity.kt
@@ -2,10 +2,21 @@ package com.cnc.kotlinbasic01_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.clickBtn
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn.setOnClickListener {
+            //코드에 메모 남기기
+            Log.d("메인화면로그","클릭용로 버튼")
+            
+        }
     }
 }
