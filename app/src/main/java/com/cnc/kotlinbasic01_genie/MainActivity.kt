9839afe4f71c596bb43ffd4847dc1018d7fe82fb
LKH <kangholee8512@gmail.com>
2023-05-02 06:38:38 +0900

    Initial commit

---
+++ app/src/main/java/com/cnc/kotlinbasic01_genie/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cnc.kotlinbasic01_genie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
